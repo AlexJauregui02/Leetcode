c903b3962259310d043060773e923ac1fe6bf318
AlexJauregui <alejandro.jauregui@encora.com>
2025-06-07 16:26:23 -0700

    Update Problem4.py

--- Python/Problem4.py
+++ Python/Problem4.py
@@ -1,1 +1,17 @@
 
+def shortestPalindrome(self, s: str) -> str:
+    reverse = s[::-1]
+    combined_string = s + '.' + reverse
+
+    lps = [0] * len(combined_string)
+
+    for i in range(1, len(combined_string)):
+        pattern = lps[i - 1]
+        while pattern > 0 and combined_string[i] != combined_string[pattern]:
+            pattern = lps[pattern - 1]
+        if combined_string[i] == combined_string[pattern]:
+            pattern += 1
+        lps[i] = pattern
+
+    chars_to_add = s[lps[-1]:]
+    return chars_to_add[::-1] + s
